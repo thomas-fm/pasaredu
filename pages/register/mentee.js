5dfa6f03e2829fc180200e9472f1026fa1f20698
thomas-fm <13519099@std.stei.itb.ac.id>
2021-06-10 13:31:00 +0700

    fix css for register&login

--- pages/register/mentee.js
+++ pages/register/mentee.js
@@ -1,14 +1,9 @@
+import React, { useEffect } from 'react'
 import {useContext, useState} from 'react'
 import {Button, Container, TextField, Icon} from '@material-ui/core'
 import {createMuiTheme, makeStyles, ThemeProvider} from '@material-ui/core/styles'
 import { useRouter } from 'next/router'
 
-const useStyles = makeStyles((theme) => ({
-    margin: {
-      margin: theme.spacing(1),
-    },
-  }));
-  
 const GoogleIcon = (props) => {
     return (
     <Icon {...props}>
@@ -24,13 +19,19 @@ const theme = createMuiTheme({
         }
     },
 });
-const Login = () => {
-    // const [userID, setID] = useContext(UserContext);
+
+const RegisterMentee = () => {
     const router = useRouter();
     const [input, setInput] = useState({
         username: "", 
-        password: ""
+        password: "",
+        errorText: null
     });
+
+    useEffect(()=>{
+
+    },[input])
+
     const handleSubmit = (event) =>{
         // butuh backend
     }
@@ -38,13 +39,25 @@ const Login = () => {
         let name = event.target.name;
         let value = event.target.value; 
 
-        setInput({...input, [name]: value});
+        if (name==="password-confirm"){
+            if (input.password !== value){
+                setInput({...input, errorText: "Password tidak cocok"})
+            }
+            else{
+                setInput({...input, errorText: null})
+            }
+        }
+        else{
+            setInput({...input, [name]: value});
+        }
+    }
+    function changeValue(e, type) {
     }
     return (
         <Container className="flex justify-center shadow-2xl" style={{background: "white", position: "fixed", top: "50%", left: "50%", flexDirection: "column", alignItems: "center", marginRight: "-50%", transform: "translate(-50%, -50%)"}} maxWidth="sm">
             <div className="flex flex-row justify-center">
             <form className="flex-col flex justify-center" noValidate autoComplete="off" style={{marginBottom: "10px"}}>
-            <h1 className="font-serif text-2xl" style={{display: "flex", justifyContent: "center"}}>Login</h1>
+            <h1 className="font-serif text-2xl" style={{display: "flex", justifyContent: "center"}}>Register</h1>
                 <div style={{display: "flex", justifyContent: "center"}}>
                     <TextField
                         id="filled-required"
@@ -53,6 +66,19 @@ const Login = () => {
                         value={input.username}
                         required
                         name="username"
+                        onChange={handleChange}
+                        className="justify-center flex"
+                        style={{width: "30ch"}}
+                    />
+                </div>
+                <div style={{display: "flex", justifyContent: "center"}}>
+                    <TextField
+                        id="filled-required"
+                        label="Fullname"
+                        placeholder="Nama Lengkap"
+                        value={input.fullname}
+                        required
+                        name="fullname"
                         onChange={handleChange}
                         className="justify-center flex"
                         style={{width: "30ch"}}
@@ -72,9 +98,22 @@ const Login = () => {
                         style={{width: "30ch"}}
                     />
                 </div>
+                <div style={{display: "flex", justifyContent: "center"}}>
+                    <TextField
+                        id="filled-required"
+                        label="Password"
+                        placeholder="Password"
+                        type="password"
+                        required
+                        name="password-confirm"
+                        onChange={handleChange}
+                        style={{width: "30ch"}}
+                        helperText={input.errorText}
+                    />
+                </div>
                 <div style={{margin: "20px 0", display: "flexbox", justifyContent: "center"}}>
                     <ThemeProvider theme={theme}>
-                    <Button style={{margin: "10px"}} variant="contained" color="primary" onClick={handleSubmit}>Login</Button>
+                    <Button style={{margin: "10px"}} variant="contained" color="primary" onClick={handleSubmit}>Register</Button>
                     <Button style={{margin: "10px"}} variant="contained" color="primary" onClick={() => router.back()}>Back to Home</Button> 
                     </ThemeProvider>               
                 </div>
@@ -90,8 +129,8 @@ const Login = () => {
                     >Masuk dengan Google</Button>
                 </div>
                 <div style={{display: "flex", justifyContent: "center"}}>
-                    <label>Belum punya akun? </label>
-                    <a href="/register/mentee" style={{paddingLeft: "5px", color: "#f4d27b", fontWeight: "bold"}}> Register</a>
+                    <label>Sudah punya akun? </label>
+                    <a href="/login" style={{paddingLeft: "5px", color: "#f4d27b", fontWeight: "bold"}}> Login</a>
                 </div>
             </form>
             </div>
@@ -99,4 +138,4 @@ const Login = () => {
     );
 }
 
-export default Login
+export default RegisterMentee
